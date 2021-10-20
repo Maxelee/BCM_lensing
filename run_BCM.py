e0190b7ab12516e6edebdf12a9845e853048ae9f
Max Lee <maxlee1993@berkeley.edu>
2021-10-20 15:49:39 -0400

    parallelized BCM working

--- run_BCM.py
+++ run_BCM.py
@@ -13,9 +13,6 @@ from mpi4py import MPI
 import illustris_python as il
 from pmesh import ParticleMesh
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string('basePath', '/burg/astro/users/mel2260/Illustris-3-Dark/output', 'Path to Illustris Data')
@@ -27,10 +24,15 @@ flags.DEFINE_float('M1', 86.3,   'M1 bcm parameter in 10^10 M_sun h^-1')
 flags.DEFINE_float('MC', 3300.0, 'MC bcm parameter in 10^10 M_sun h^-1')
 flags.DEFINE_float('eta', 0.54, 'eta bcm parameter')
 flags.DEFINE_float('beta', 0.12, 'beta bcm parameter')
+flags.DEFINE_boolean('DMO', False, 'whether to compute DMO power spectrum or not')
 
 
 def main(argv):
     del argv
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size= comm.Get_size()
 
     # Illustris Data
     subgroupFields = ['SubhaloPos']
@@ -48,9 +50,9 @@ def main(argv):
     bcm_pos = np.ones((1, 3))
     counter = 0
     bcm_start = time.time()
-    print('starting')    
-    # Do the BCM on each of halos
-    for halo_num in range(FLAGS.num_halos):
+    perrank = FLAGS.num_halos // size
+    # Do the BCM on each of halos, parallelized
+    for halo_num in range(rank * perrank, (rank + 1) * perrank):
         h_coords= BCM_POS(group_df, halo_num,groupPos, subgroupPos, 
                 FLAGS.basePath, constraint=FLAGS.constraint, 
                 M1=FLAGS.M1, MC=FLAGS.MC, eta=FLAGS.eta, beta=FLAGS.beta)
@@ -58,22 +60,28 @@ def main(argv):
         counter += len(h_coords)
 
     print(f'\nBCM finished in {time.time()-bcm_start} seconds')
-
+    comm.Barrier()
     # Now replace the snapshots halos with BCM corrected halos
     bcm_pos  = bcm_pos[1:]
-    dat = il.snapshot.loadSubset(FLAGS.basePath, FLAGS.snapNum, 'dm',  'Coordinates')
-    dat[:counter] = bcm_pos
-
-    # Put in box coordinates (Mpc/h)
-    dat /= 1000
-    dat %= 75
-    dat[np.isnan(dat)] =0
+    bcm_pos = comm.gather(bcm_pos, root=0)
+    counter = comm.reduce(counter, root=0, op=MPI.SUM)
     
+    if rank ==0:
+        bcm_pos = np.vstack(bcm_pos)
+        dat = il.snapshot.loadSubset(FLAGS.basePath, FLAGS.snapNum, 'dm',  'Coordinates')
+        dat[:counter] = bcm_pos
+
+        # Put in box coordinates (Mpc/h)
+        dat /= 1000
+        dat %= 75
+        dat[np.isnan(dat)] =0
+    else:
+        dat = None
+
+    dat = comm.bcast(dat, root=0)
     # Put into a particle mesh object for mapping and power spectrum
     pm = ParticleMesh(Nmesh=[455]*3, BoxSize=[75, 75, 75], resampler='cic')
-    comm = pm.comm
-    mass1 = 1.0 * pm.Nmesh.prod() / pm.comm.allreduce(len(dat), op=MPI.SUM) * 4.8e-2
-
+    mass1 =4.8e-2# 1.0 * pm.Nmesh.prod() / pm.comm.allreduce(len(dat), op=MPI.SUM) * 4.8e-2
 
     IllustrisMap = pm.create(type="real")
     IllustrisMap.paint(dat, mass=mass1, layout=None, hold=False)
@@ -91,29 +99,33 @@ def main(argv):
     P = FFTPower(IllustrisMap, mode='1d').save(address_BCM +'_p.json')
 
     """-------------------------------------------DMO-----------------------------"""
+    if FLAGS.DMO:
 
-    address_DMO = address + 'power/' +'DMOmap_Nmesh' + str(pm.Nmesh[0])
+        address_DMO = address + 'power/' +'DMOmap_Nmesh' + str(pm.Nmesh[0])
 
-    try:
-        IllustrisMap_DMO = BigFileMesh(address_DMO, dataset='Field').to_real_field()
-    except:
+        try:
+            IllustrisMap_DMO = BigFileMesh(address_DMO, dataset='Field').to_real_field()
+        except:
+            if rank == 0:
+                dat = il.snapshot.loadSubset(FLAGS.basePath, FLAGS.snapNum, 'dm',  'Coordinates')
+                dat /= 1000
+                dat %= 75
+            else:
+                dat = None
 
-        dat = il.snapshot.loadSubset(FLAGS.basePath, FLAGS.snapNum, 'dm',  'Coordinates')
-        dat /= 1000
-        dat %= 75
+            dat = comm.bcast(dat, root=0)
+            mass1 = 4.8e-2#1.0 * pm.Nmesh.prod() / pm.comm.allreduce(len(dat), op=MPI.SUM) * 4.8e-2
 
-        mass1 = 1.0 * pm.Nmesh.prod() / pm.comm.allreduce(len(dat), op=MPI.SUM) * 4.8e-2
+            IllustrisMap_DMO = pm.create(type="real")
+            IllustrisMap_DMO.paint(dat, mass=mass1, layout=None, hold=False)
+            del dat, mass1,
 
-        IllustrisMap_DMO = pm.create(type="real")
-        IllustrisMap_DMO.paint(dat, mass=mass1, layout=None, hold=False)
-        del dat, mass1
+            #FieldMesh(IllustrisMap_DMO).save(address_DMO)
 
-        FieldMesh(IllustrisMap_DMO).save(address_DMO)
+            P_DMO = FFTPower(IllustrisMap_DMO, mode='1d').save(address_DMO +'_p.json')
 
-        P_DMO = FFTPower(IllustrisMap_DMO, mode='1d').save(address_DMO +'_p.json')
-
-    # Cross correlation coefficient incase this is a useful statistic
-    r_cc = FFTPower(IllustrisMap, second=IllustrisMap_DMO, mode='1d').save(address_BCM +'_rcc.json')
+        # Cross correlation coefficient incase this is a useful statistic
+        r_cc = FFTPower(IllustrisMap, second=IllustrisMap_DMO, mode='1d').save(address_BCM +'_rcc.json')
 
 
 if __name__ == "__main__":
